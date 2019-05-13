9e9c37497dc447ab165b7df1be56171850aae88f
Jennifer Scheinhorn <jscheinhorn@gmail.com>
2019-05-13 10:29:09 -0400

    made candy model

--- server/db/models/Candy.js
+++ server/db/models/Candy.js
@@ -3,4 +3,28 @@ const db = require('../database');
 
 module.exports = db.define('candy', {
   // define your model here!
+  name: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    validate: {
+      max: 10
+    }
+  },
+  imageURL: {
+    type: Sequelize.TEXT,
+    defaultValue: 'someImage'
+  }
 });
